a6e1ba8e18806bfb695510d0beb8ca7f55dc8749
Mathis Keller <mk@mathiskeller.de>
2017-03-16 19:38:48 +0100

    Added Instantiate function.

---
+++ Assets/Scripts/InstantiateBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBall : MonoBehaviour {
+	public GameObject prefab;
+	public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			CreateBall ();
+		}
+	}
+
+	void CreateBall () {
+		GameObject ball = Instantiate (prefab, position, Quaternion.identity);
+
+		// Set random Color
+		Material mat = ball.GetComponent<MeshRenderer> ().materials [0];
+		Color color = Random.ColorHSV();
+
+		mat.color = color;
+	}
+}
